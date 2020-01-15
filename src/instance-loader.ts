d4af96c0b7d2afc19497c5b79638eed8226817ec
Magna Marek <magna.marek@pbs.one>
2020-01-15 23:18:45 +0100

    [polish] minor type simplification @ instance-loader #12

--- src/instance-loader.ts
+++ src/instance-loader.ts
@@ -7,20 +7,20 @@ import { Property } from "./property";
 import { Primitive, Unbox } from "./lang";
 
 export interface InstanceLoader<T extends SourceType> {
-    load(loadable: InstanceLoader.Loadable<T, "loadable">, criteria: ObjectCriterion.ForType<T>[]): Map<string | number, Instance<T, "loadable">>;
+    load(loadable: InstanceLoader.Loadable<T>, criteria: ObjectCriterion.ForType<T>[]): Map<string | number, Instance<T, "loadable">>;
 }
 
 export module InstanceLoader {
     type LoadableProperty<P extends Property & Context.Has<C>, C extends Context>
         = P["value"] extends Primitive ? P[C]["optional"] extends true ? Context.ChangeOptional<P, C, boolean> : P
-        : Property.ReplaceValue<P[C]["optional"] extends true ? Context.ChangeOptional<P, C, boolean> : P, Loadable<Unbox<P["value"]>, C>>;
+        : Property.ReplaceValue<P[C]["optional"] extends true ? Context.ChangeOptional<P, C, boolean> : P, Loadable<Unbox<P["value"]>>>;
 
-    export type Loadable<ST extends SourceType, CTX extends Context>
+    export type Loadable<ST extends SourceType>
         = TappedType<ST>
         & {
-            [K in Property.Keys<ST, Context.IsRequired<CTX>>]: LoadableProperty<ST[K], CTX>;
+            [K in Property.Keys<ST, Context.IsRequired<"loadable">>]: LoadableProperty<ST[K], "loadable">;
         }
         & {
-            [K in Property.Keys<ST, Context.IsOptional<CTX>>]?: LoadableProperty<ST[K], CTX>;
+            [K in Property.Keys<ST, Context.IsOptional<"loadable">>]?: LoadableProperty<ST[K], "loadable">;
         };
 }
