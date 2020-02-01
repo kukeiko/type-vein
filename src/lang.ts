39b26008576a68d28e4daa76c5741544d4683748
Magna Marek <magna.marek@pbs.one>
2020-02-01 22:08:15 +0100

    [mod] make Class type generic

--- src/lang.ts
+++ src/lang.ts
@@ -17,7 +17,7 @@ export module Primitive {
 /**
  * A type that has a constructor.
  */
-export type Class = new (...args: any) => any;
+export type Class<T = any> = new (...args: any) => T;
 
 /**
  * Takes a type T and returns:
